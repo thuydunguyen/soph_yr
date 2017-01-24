7f6450c4fb7dcdabc1facbda2390d7ef7c6c339c
Thuy-Du Nguyen <Thuy-Du Nguyen>
2017-01-23 21:08:59 -0800

    Nbody.java - ReadPlanets done

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -1,3 +1,31 @@
 public class NBody {
 
+
+	public static double readRadius(String args) {
+
+		In in = new In(args);
+		int num_pl = in.readInt();
+		double radius = in.readDouble();
+		return radius;}
+
+	public static Planet[] readPlanets(String args) {
+		In in = new In(args);
+		int num_pl = in.readInt();
+		double radius = in.readDouble();
+		Planet[] list = new Planet[num_pl];
+		for (int n=0; n < num_pl; n+=1) {
+				double xP = in.readDouble();
+				double yP = in.readDouble();
+				double xV = in.readDouble();
+				double yV = in.readDouble();
+				double mass = in.readDouble();
+				String imgfile = in.readString();
+				Planet pl = new Planet(xP, yP, xV, yV, mass, imgfile);
+				list[n] = pl;}
+		return list;}
+
+
+
+
+
 }
