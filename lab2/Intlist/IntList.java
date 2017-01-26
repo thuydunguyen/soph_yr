9264446747d17f064203b2bebaff5732c854388a
Thuy-Du Nguyen <Thuy-Du Nguyen>
2017-01-26 13:49:05 -0800

    Lab2 complete

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -81,7 +81,11 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        IntList C = A;
+        while (C.rest != null) {
+            C = C.rest;}
+        C.rest = B;
+        return A;
     }
 
     /**
@@ -90,7 +94,10 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if (A.rest == null) {
+            return new IntList(A.first, B);}
+        return new IntList(A.first, catenate(A.rest, B));
+
     }
 
 
