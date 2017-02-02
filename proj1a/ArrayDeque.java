adead8482a9ed4e3d2a9c16aa4e1d5cd2dfbbbab
Thuy-Du Nguyen <Thuy-Du Nguyen>
2017-02-02 13:54:47 -0800

    Proj 1a

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -3,7 +3,7 @@ public class ArrayDeque<Item> {
     private int size;
 
     public ArrayDeque() {
-        items = (Item[]) new Object[10];
+        items = (Item[]) new Object[1];
         size = 0;
     }
 
@@ -17,21 +17,20 @@ public class ArrayDeque<Item> {
     public void addFirst(Item x) {
 
         if (size == items.length) {
-            resize(size ^ 2);
+            resize(size + 1);
         }
 
         Item[] a = (Item[]) new Object[items.length];
         System.arraycopy(items, 0, a, 1, size);
         items = a;
         items[0] = x;
-        size += 1;
-    }
+        size += 1;}
 
 
     public void addLast(Item x) {
-
+        /* last item goes into position "size" */
         if (size == items.length) {
-            resize(size ^ 2);
+            resize(size + 1);
         }
 
         items[size] = x;
@@ -41,8 +40,8 @@ public class ArrayDeque<Item> {
 
     public Item get(int index) {
 
-        if (size < index) {
-            return null;
+    	if (size < index) {
+    	    return null;
         }
         return items[index];
     }
@@ -55,16 +54,28 @@ public class ArrayDeque<Item> {
         return size;
     }
 
+    public Item removeLast() {
+
+        if (isEmpty()) {
+            return null;
+        }
+
+        size -= 1;
+        Item x = items[size];
+        items[size] = null;
+        return x;
+    }
 
     public void printDeque() {
         int n = 0;
 
         while (n < size) {
-            if (n == (size - 1)) {
+            if (n == (size-1)) {
                 System.out.println(items[n]);
-            } else {
-                System.out.print(items[n] + " ");
             }
+                else {
+                    System.out.print(items[n] + " ");
+                }
             n += 1;
         }
     }
@@ -76,24 +87,10 @@ public class ArrayDeque<Item> {
         }
 
         Item x = items[0];
-
         size -= 1;
-        Item[] a = (Item[]) new Object[size * 2];
+        Item[] a = (Item[]) new Object[size];
         System.arraycopy(items, 1, a, 0, size);
         items = a;
-        return x;
-    }
-
-    public Item removeLast() {
-
-        if (isEmpty()) {
-            return null;
-        }
-
-        size -= 1;
-        Item x = items[size];
-        items[size] = null;
-        resize(size * 2);
         return x;
     }
 
