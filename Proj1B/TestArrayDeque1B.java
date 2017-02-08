46829e4c92019f2a4c92743ce876d20ae75c2ccb
Thuy-Du Nguyen <Thuy-Du Nguyen>
2017-02-08 04:55:28 -0800

    Proj1b complete

--- Proj1B/TestArrayDeque1B.java
+++ Proj1B/TestArrayDeque1B.java
@@ -12,7 +12,7 @@ import java.util.ArrayDeque;
 public class TestArrayDeque1B {
 
     @Test
-    public void Phase1_1() {
+    public void phase1() {
         StudentArrayDeque<Integer> t = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> s = new ArrayDequeSolution<>();
         boolean x = true;
@@ -22,16 +22,16 @@ public class TestArrayDeque1B {
         DequeOperation op = new DequeOperation("initialize");
 
         while (x) {
-            int func_call = StdRandom.uniform(150);
+            int func = StdRandom.uniform(150);
             int num = StdRandom.uniform(50);
-            if (func_call < 25) {
+            if (func < 25) {
                 op = new DequeOperation("addFirst", num);
                 s.addFirst(num);
                 t.addFirst(num);
-                s1 = s.get(0);
+                s1 = s.getFirst();
                 t1 = t.get(0);
                 x = s1 == t1;
-            } else if (func_call < 50) {
+            } else if (func < 50) {
                 s1 = s.removeFirst();
                 t1 = t.removeFirst();
                 if ((s.size() == 0) || (s.size() != t.size())) {
@@ -43,15 +43,14 @@ public class TestArrayDeque1B {
                     op = new DequeOperation("removeFirst");
                     x = s1 == t1;
                 }
-
-            } else if (func_call < 75) {
+            } else if (func < 75) {
                 op = new DequeOperation("addLast", num);
                 s.addLast(num);
                 t.addLast(num);
-                s1 = s.get(s.size() - 1);
+                s1 = s.getLast();
                 t1 = t.get(t.size() - 1);
                 x = s1 == t1;
-            } else if (func_call < 100) {
+            } else if (func < 100) {
                 s1 = s.removeLast();
                 t1 = t.removeLast();
                 if ((s.size() == 0) || (s.size() != t.size())) {
@@ -63,12 +62,12 @@ public class TestArrayDeque1B {
                     op = new DequeOperation("removeLast");
                     x = s1 == t1;
                 }
-            } else if (func_call < 125) {
+            } else if (func < 125) {
                 op = new DequeOperation("size");
                 s1 = s.size();
                 t1 = t.size();
                 x = s1 == t1;
-            } else if (func_call < 150) {
+            } else if (func < 150) {
                 if ((s.size() == 0) || (s.size() != t.size())) {
                     x = t.size() == s.size();
                     op = new DequeOperation("size");
