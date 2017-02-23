a8db8456f679ae84028334dfa42eed27aebafe89
Thuy-Du Nguyen <Thuy-Du Nguyen>
2017-02-22 22:35:14 -0800

    Hw 1

--- hw1/synthesizer/GuitarString.java
+++ hw1/synthesizer/GuitarString.java
@@ -1,8 +1,7 @@
-// TODO: Make sure to make this class a part of the synthesizer package
+
 //package <package name>;
 package synthesizer;
 
-import java.util.stream.DoubleStream;
 
 //Make sure this class is public
 public class GuitarString {
@@ -19,7 +18,7 @@ public class GuitarString {
 
     /* Create a guitar string of the given frequency.  */
     public GuitarString(double frequency) {
-        // TODO: Create a buffer with capacity = SR / frequency. You'll need to
+
         //       cast the result of this division operation into an int. For better
         //       accuracy, use the Math.round() function before casting.
         //       Your buffer should be initially filled with zeros.
@@ -30,7 +29,7 @@ public class GuitarString {
 
     /* Pluck the guitar string by replacing the buffer with white noise. */
     public void pluck() {
-        // TODO: Dequeue everything in the buffer, and replace it with random numbers
+
         //       between -0.5 and 0.5. You can get such a number by using:
         //       double r = Math.random() - 0.5;
         //
@@ -48,7 +47,7 @@ public class GuitarString {
      * the Karplus-Strong algorithm. 
      */
     public void tic() {
-        // TODO: Dequeue the front sample and enqueue a new sample that is
+
         //       the average of the two multiplied by the DECAY factor.
         //       Do not call StdAudio.play().
         Double item = buffer.dequeue();
@@ -60,7 +59,7 @@ public class GuitarString {
 
     /* Return the double at the front of the buffer. */
     public double sample() {
-        // TODO: Return the correct thing.
+
         return buffer.peek();
     }
 }
