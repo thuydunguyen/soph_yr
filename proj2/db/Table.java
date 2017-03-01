21d64ca031d752f5e36fbdb1a67dc764b24a1839
Thuy-Du Nguyen <thuydumailbox@berkeley.edu>
2017-02-28 16:30:09 -0800

    Table class update1

--- proj2/db/Table.java
+++ proj2/db/Table.java
@@ -1,36 +1,67 @@
 package db;
-import java.util.ArrayList;
+import java.util.*;
 
 /**
  * Created by Thuy-Du on 2/26/2017.
  */
 public class Table<T> {
     private boolean CrTableSel = false;
-    private ArrayList<ArrayList> col;
+    private ArrayList<ArrayList<String>> table;
 
     public Table() {
-        col = new ArrayList<>();
+        table = new ArrayList<>();
     }
 
     public Table(int cols, String[] col_names, String[] col_types) {
-        col = new ArrayList<>(col_names.length);
+        table = new ArrayList<>(col_names.length);
         for (int x = 0; x < col_names.length; x++) {
-            ArrayList<String> sets = new ArrayList<String>();
-            sets.add(0, col_names[x]);
-            sets.add(1, col_types[x]);
-            col.set(x, sets);
+            ArrayList<String> sets = new ArrayList<>();
+            sets.add(col_names[x]);
+            sets.add(col_types[x]);
+            table.add(sets);
         }
     }
 
     public void insert(T[] data) {
-        for (int x = 0; x < col.size() + 1; x++) {
-            ArrayList copy = col.get(x);
-            copy.add(data[x]);
-            col.set(x, copy);
+        for (int x = 0; x < table.size(); x++) {
+            ArrayList copy = table.get(x);
+            String point = data[x].toString();
+            copy.add(point);
+            table.set(x, copy);
         }
     }
 
-    public void select_join(String[] col_names) {
+    public void print() {
+        //Prints first n-1 col_names with col_types
+        String comma;
+        for (int x = 0; x < table.size(); x++) {
+            if (x == table.size() - 1) {
+                comma = "";
+                System.out.println(table.get(x).get(0) + " " + table.get(x).get(1) + comma);
 
+            } else {
+                comma = ",";
+                System.out.print(table.get(x).get(0) + " " + table.get(x).get(1) + comma);
+            }
+        }
+        for (int y = 2; y < table.get(0).size(); y++) {
+            for (int x = 0; x < table.size(); x++) {
+                if (x == table.size() - 1) {
+                    comma = "";
+                    System.out.println(table.get(x).get(y) + comma);
+
+                } else {
+                    comma = ",";
+                    System.out.print(table.get(x).get(y) + comma);
+                }
+            }
+        }
+    }
+    //This main method is for testing.
+    public static void main(String[] args) {
+        Table t = new Table(3, new String[] {"a", "b", "c"}, new String[] {"String", "String", "String"});
+        t.insert(new Integer[] {1,2,3});
+        t.insert(new Integer[] {4,5,6});
+        t.print();
     }
 }
