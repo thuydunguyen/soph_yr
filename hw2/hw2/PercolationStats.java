b0787b637ec6b44e248858f6330f76e90750e664
Thuy-Du Nguyen <thuydumailbox@berkeley.edu>
2017-03-15 17:21:56 -0700

    Hw 2 Debugging timeouts

--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -24,7 +24,6 @@ public class PercolationStats {
                     n++;
                 }
             }
-            System.out.println(x + ": " + n);
             results[x] = (n / (N * N));
         }
         tests = T;
