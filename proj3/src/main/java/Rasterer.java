5b2cb54eacfcc5eb37961729b59d03f89c2a838d
Thuy-Du Nguyen <thuydumailbox@berkeley.edu>
2017-04-19 04:32:40 -0700

    Proj 3 updated

--- proj3/src/main/java/Rasterer.java
+++ proj3/src/main/java/Rasterer.java
@@ -45,110 +45,99 @@ public class Rasterer {
             double r_ullon = MapServer.ROOT_ULLON;
             double r_lrlon = MapServer.ROOT_LRLON;
             this.img = img;
-            if (img.length() == 1) {
-                parent = null;
-            } else {
-                String p = img.substring(0, img.length() - 1);
-                parent = QuadTree.get(p);
-            }
-            if (img.length() == 1) {
+            if (img.equals("root")) {
+                parent = this;
                 ullon = r_ullon;
                 ullat = r_ullat;
                 lrlon = r_lrlon;
                 lrlat = r_lrlat;
+            } else if (img.length() == 1) {
+                parent = QuadTree.get("root");
             } else {
+                String p = img.substring(0, img.length() - 1);
+                parent = QuadTree.get(p);
+            }
+            if (!img.equals("root")) {
                 String l_num = img.substring(img.length() - 1);
                 double[][][] coords = coords(parent.ullon, parent.ullat, parent.lrlon, parent.lrlat);
                 if (l_num.equals("1")) {
                     ullon = coords[0][0][0];
                     ullat = coords[0][0][1];
-                    lrlat = coords[1][1][0];
-                    lrlon = coords[1][1][1];
+                    lrlat = coords[1][1][1];
+                    lrlon = coords[1][1][0];
                 } else if (l_num.equals("2")) {
                     ullon = coords[0][1][0];
                     ullat = coords[0][1][1];
-                    lrlat = coords[1][2][0];
-                    lrlon = coords[1][2][1];
+                    lrlat = coords[1][2][1];
+                    lrlon = coords[1][2][0];
                 } else if (l_num.equals("3")) {
                     ullon = coords[1][0][0];
                     ullat = coords[1][0][1];
-                    lrlat = coords[2][1][0];
-                    lrlon = coords[2][1][1];
+                    lrlat = coords[2][1][1];
+                    lrlon = coords[2][1][0];
                 } else if (l_num.equals("4")) {
                     ullon = coords[1][1][0];
                     ullat = coords[1][1][1];
-                    lrlat = coords[2][2][0];
-                    lrlon = coords[2][2][1];
+                    lrlat = coords[2][2][1];
+                    lrlon = coords[2][2][0];
                 }
             }
             placeZoom(img, this);
             QuadTree.put(img, this);
+            if (img.equals("root")) {
+                img = "";
+            }
             for (int x = 0; x < 4; x++) {
                 if (img.length() == 7) {
                     sub[x] = null;
                 } else {
                     sub[x] = new Node(img + num[x]);
-                    sub[x].parent = this;
                 }
             }
 
 
         }
+
 
         double[][][] coords(double ullon, double ullat, double lrlon, double lrlat) {
             double m_lon = (ullon + lrlon) / 2;
             double m_lat = (ullat + lrlat) / 2;
-            double[] x_y = new double[2];
+            double[] longs = new double[]{ullon, m_lon, lrlon};
+            double[] lats = new double[]{ullat, m_lat, lrlat};
             double[][][] coord = new double[3][3][2];
             for (int x = 0; x < 3; x++) {
-                if (x == 0) {
-                    x_y[1] = ullat;
-                } else if (x == 1) {
-                    x_y[1] = m_lat;
-                } else {
-                    x_y[1] = lrlat;
-                }
                 for (int y = 0; y < 3; y++) {
-                    if (y == 0) {
-                        x_y[0] = ullon;
-                    } else if (y == 1) {
-                        x_y[0] = m_lon;
-                    } else {
-                        x_y[0] = lrlon;
-                    }
-                    coord[x][y] = x_y;
+                    coord[x][y][0] = longs[y];
+                    coord[x][y][1] = lats[x];
                 }
             }
             return coord;
-        }
 
+        }
     }
 
     private void placeZoom(String img, Node t) {
         int len = img.length();
-        switch (len) {
-            case 1:
-                Zoom1.add(t);
-            case 2:
-                Zoom2.add(t);
-            case 3:
-                Zoom3.add(t);
-            case 4:
-                Zoom4.add(t);
-            case 5:
-                Zoom5.add(t);
-            case 6:
-                Zoom6.add(t);
-            case 7:
-                Zoom7.add(t);
+        if (len == 1) {
+            Zoom1.add(t);
+        } else if (len == 2) {
+            Zoom2.add(t);
+        } else if (len == 3) {
+            Zoom3.add(t);
+        } else if (len == 4) {
+            Zoom4.add(t);
+        } else if (len == 5) {
+            Zoom5.add(t);
+        } else if (len == 6) {
+            Zoom6.add(t);
+        } else if (len == 7) {
+            Zoom7.add(t);
         }
     }
 
 
     public Rasterer(String imgRoot) {
-        for (int x = 0; x < 4; x++) {
-            new Node(num[x]);
-        }
+        new Node("root");
         Zooms.add(Zoom1);
         Zooms.add(Zoom2);
         Zooms.add(Zoom3);
@@ -208,52 +197,91 @@ public class Rasterer {
         Double d_width = params.get("w");
         Double d_ullat = params.get("ullat");
         Double d_lrlat = params.get("lrlat");
-        double d_londpp = LonDPP(d_lrlon, d_ullon, d_width);
-        Node first;
-        Node sec;
-        Node third;
-        Node curr = QuadTree.get("1");
-        double londpp = LonDPP(curr.lrlon, curr.ullon, d_width);
-        while (londpp > d_londpp) {
-            curr = curr.sub[0];
-            londpp = LonDPP(curr.lrlon, curr.ullon, d_width);
-        }
-        int len = curr.img.length();
-        first = Zooms.get(len - 1).get(0);
-        sec = Zooms.get(len - 1).get(0);
-        third = Zooms.get(len - 1).get(0);
-        for (int x = 0; x < Zooms.get(len - 1).size(); x++) {
-            Node check = Zooms.get(len - 1).get(x);
-            if (contained(d_ullon, d_ullat, check)) {
-                first = check;
+        if (!valid(d_ullon, d_lrlon, d_ullat, d_lrlat)) {
+            results.put("query_succes", false);
+        } else {
+            double d_londpp = LonDPP(d_lrlon, d_ullon, d_width);
+            d_lrlon = adjust(d_lrlon, "lrlon");
+            d_lrlat = adjust(d_lrlat, "lrlat");
+            d_ullon = adjust(d_ullon, "ullon");
+            d_ullat = adjust(d_ullat, "ullat");
+            Node first;
+            Node sec;
+            Node third;
+            Node curr = QuadTree.get("1");
+            double londpp = LonDPP(curr.lrlon, curr.ullon, 256);
+            while (londpp > d_londpp) {
+                curr = curr.sub[0];
+                londpp = LonDPP(curr.lrlon, curr.ullon, 256);
             }
-            if (contained(d_ullon, d_lrlat, check)) {
-                third = check;
+            int len = curr.img.length();
+            int k = 0;
+            Node check = Zooms.get(len - 1).get(k);
+            while (!contained(d_ullon, d_ullat, check)) {
+                k++;
+                check = Zooms.get(len - 1).get(k);
             }
-            if (contained(d_lrlon, d_ullat, check)) {
-                sec = check;
+            first = check;
+
+            ArrayList<String> col = nimgs(first.img, d_lrlat, d_lrlon, "v");
+            int n_rows = col.size();
+            ArrayList<String> row = nimgs(first.img, d_lrlat, d_lrlon, "h");
+            int n_cols = row.size();
+            imgs = new String[n_rows][n_cols];
+            String[] rowed = row.toArray(new String[n_rows]);
+            imgs[0] = rowed;
+            for (int x = 1; x < col.size(); x++) {
+                Node next = QuadTree.get(col.get(x));
+                row = imgrows(next.img, n_cols);
+                rowed = row.toArray(new String[n_rows]);
+                imgs[x] = rowed;
             }
+            sec = QuadTree.get(imgs[0][n_cols - 1]);
+            third = QuadTree.get(imgs[n_rows - 1][0]);
+
+            results.put("rendered_grid", imgs);
+            results.put("raster_ul_lon", first.ullon);
+            results.put("raster_ul_lat", first.ullat);
+            results.put("raster_lr_lon", sec.lrlon);
+            results.put("raster_lr_lat", third.lrlat);
+            results.put("depth", len);
+            results.put("query_success", true);
         }
-        ArrayList<String> col = nimgs(first.img, third.img, "v");
-        int n_rows = col.size();
-        ArrayList<String> row = nimgs(first.img, sec.img, "h");
-        int n_cols = row.size();
-        imgs = new String[n_rows][n_cols];
-        String[] rowed = row.toArray(new String[n_rows]);
-        imgs[0] = rowed;
-        for (int x = 1; x < col.size(); x++) {
-            Node next = QuadTree.get(col.get(x));
-            row = nimgs(next.img, sec.img, "h");
-            rowed = row.toArray(new String[n_rows]);
-            imgs[x] = rowed;
-        }
-        results.put("rendered_grid", imgs);
-        results.put("raster_ul_lon", first.ullon);
-        results.put("raster_ul_lat", first.ullat);
-        results.put("raster_lr_lon", sec.lrlon);
-        results.put("raster_lr_lat", third.lrlat);
-        results.put("depth", len);
         return results;
+    }
+
+    private double adjust(double d, String point) {
+        double r_ullat = MapServer.ROOT_ULLAT;
+        double r_lrlat = MapServer.ROOT_LRLAT;
+        double r_ullon = MapServer.ROOT_ULLON;
+        double r_lrlon = MapServer.ROOT_LRLON;
+        if (point.equals("ullon")) {
+            if (d < r_ullon) {
+                return r_ullon;
+            }
+        } else if (point.equals("ullat")) {
+            if (d > r_ullat) {
+                return r_ullat;
+            }
+        } else if (point.equals("lrlon")) {
+            if (d > r_lrlon) {
+                return r_lrlon;
+            }
+        } else if (point.equals("lrlat")) {
+            if (d < r_lrlat) {
+                return r_lrlat;
+            }
+        }
+        return d;
+    }
+
+    private boolean contained(double lon, double lat, Node t) {
+        if (t.ullon <= lon && t.lrlon >= lon) {
+            if (t.ullat >= lat && t.lrlat <= lat) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private static String incr(String img, String horv) {
@@ -287,23 +315,53 @@ public class Rasterer {
         return x;
     }
 
-    private ArrayList<String> nimgs(String img, String img2, String horv) {
-        ArrayList<String> n = new ArrayList<>();
-        n.add(QuadTree.get(img).img);
-        while (!img.equals(img2)) {
-            img = incr(img, horv);
-            n.add(QuadTree.get(img).img);
+    private ArrayList<String> nimgs(String img, double d_lrlat, double d_lrlon, String horv) {
+        ArrayList<String> row = new ArrayList<>();
+        ArrayList<String> col = new ArrayList<>();
+        Node curr = QuadTree.get(img);
+        String comp2 = "\\b[2,4]+\\b";
+        String comp3 = "\\b[3,4]+\\b";
+        col.add(curr.img);
+        row.add(curr.img);
+        if (horv.equals("h")) {
+            while (curr.lrlon < d_lrlon && !curr.img.matches(comp2)) {
+                curr = QuadTree.get(incr(curr.img, "h"));
+                row.add(curr.img);
+            }
+            return row;
+        } else {
+            while (curr.lrlat > d_lrlat && !curr.img.matches(comp3)) {
+                curr = QuadTree.get(incr(curr.img, "v"));
+                col.add(curr.img);
+            }
+            return col;
         }
-        return n;
+
     }
 
-    private boolean contained(double dlon, double dlat, Node t) {
-        if (dlon > t.ullon && dlon < t.lrlon) {
-            if (dlat < t.ullat && dlat > t.lrlat) {
-                return true;
-            }
+    private ArrayList<String> imgrows(String img, int times) {
+        ArrayList<String> row = new ArrayList<>();
+        Node node = QuadTree.get(img);
+        String curr = node.img;
+        row.add(curr);
+        for (int x = 0; x < times; x++) {
+            row.add(incr(curr, "h"));
         }
-        return false;
+        return row;
+    }
+
+    private boolean valid(double ullon, double lrlon, double ullat, double lrlat) {
+        double r_ullat = MapServer.ROOT_ULLAT;
+        double r_lrlat = MapServer.ROOT_LRLAT;
+        double r_ullon = MapServer.ROOT_ULLON;
+        double r_lrlon = MapServer.ROOT_LRLON;
+        if (ullon > lrlon || lrlat > ullat) {
+            return false;
+        } else if (r_ullon > ullon && r_lrlon < lrlon && r_ullat < ullat && r_lrlat > lrlat) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
     public static void main(String[] args) {
@@ -317,18 +375,33 @@ public class Rasterer {
         params.put("lrlat", 37.8318576119893);
         Map<String, Object> k = t.getMapRaster(params);
         String[][] imgs = (String[][]) k.get("rendered_grid");
-        //for (int x = 0; x < imgs.length; x++) {
-        //   for (int y = 0; y < imgs[0].length; y++ ) {
-        //        System.out.print(imgs[x][y] + "  ");
-        //    }
-        //    System.out.println("");
-        //}
+        for (int x = 0; x < imgs.length; x++) {
+            for (int y = 0; y < imgs[0].length; y++) {
+                System.out.print(imgs[x][y] + "  ");
+            }
+            System.out.println("");
+        }
 
-        System.out.println(t.QuadTree.get("1").ullon);
-        System.out.println(t.QuadTree.get("11").ullon);
-        System.out.println(t.QuadTree.get("12").ullon);
-        System.out.println(t.QuadTree.get("13").ullon);
-        System.out.println(t.QuadTree.get("14").ullon);
+
+        double r_lrlon = -122.2104604264636;
+        double r_ullon = -122.30410170759153;
+        double r_ullat = 37.870213571328854;
+        double r_lrlat = 37.8318576119893;
+        r_lrlon = t.adjust(r_lrlon, "lrlon");
+        r_lrlat = t.adjust(r_lrlat, "lrlat");
+        r_ullon = t.adjust(r_ullon, "ullon");
+        r_ullat = t.adjust(r_ullat, "ullat");
+
+
+        /**double lrlon = t.QuadTree.get("11").lrlon;
+         double ullon = t.QuadTree.get("11").ullon;
+         double ullat = t.QuadTree.get("11").ullat;
+         double lrlat = t.QuadTree.get("11").lrlat;
+         */
+
+        Node a = t.QuadTree.get("11");
+        Node b = t.QuadTree.get("13");
+        System.out.print(t.contained(r_ullon, r_ullat, a));
 
 
     }
