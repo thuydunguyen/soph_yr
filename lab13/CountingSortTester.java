eea68de10e44c25c7c4e31b8e696a5da32b6d37c
Thuy-Du Nguyen <thuydumailbox@berkeley.edu>
2017-04-20 21:06:58 -0700

    Lab 13

--- lab13/CountingSortTester.java
+++ lab13/CountingSortTester.java
@@ -1,4 +1,5 @@
 import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 public class CountingSortTester {
@@ -12,44 +13,44 @@ public class CountingSortTester {
      * Array that will cause CountingSort.naiveCountingSort to fail, but CountingSort.betterCountingSort can handle
      **/
     public static int[] someNegative = {9, 5, -4, 2, 1, -2, 5, 3, 0, -2, 3, 1, 1};
-    
+
     public static void assertIsSorted(int[] a) {
-    	int previous = Integer.MIN_VALUE;
-    	for (int x : a) {
-    		assertTrue(x >= previous);
-    		previous = x;
-    	}
+        int previous = Integer.MIN_VALUE;
+        for (int x : a) {
+            assertTrue(x >= previous);
+            previous = x;
+        }
     }
 
-	@Test
-	public void testNaiveWithNonNegative() {
-		int[] sortedNonNegative = CountingSort.naiveCountingSort(nonNegative);
-		assertIsSorted(sortedNonNegative);
-	}
+    @Test
+    public void testNaiveWithNonNegative() {
+        int[] sortedNonNegative = CountingSort.naiveCountingSort(nonNegative);
+        assertIsSorted(sortedNonNegative);
+    }
 
-	@Test
-	public void testNaiveWithSomeNegative() {
-		try {
-			int[] sortedSomeNegative = CountingSort.naiveCountingSort(someNegative);
-		} catch (ArrayIndexOutOfBoundsException e) {
-			System.out.println("Great! Exception happened as we expected, since this sort is broken for negative numbers.");
-		}
-	}
+    @Test
+    public void testNaiveWithSomeNegative() {
+        try {
+            int[] sortedSomeNegative = CountingSort.naiveCountingSort(someNegative);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Great! Exception happened as we expected, since this sort is broken for negative numbers.");
+        }
+    }
 
-	@Test
-	public void testBetterWithNonNegative() {
-		int[] sortedNonNegative = CountingSort.betterCountingSort(nonNegative);
-		assertIsSorted(sortedNonNegative);
-	}
+    @Test
+    public void testBetterWithNonNegative() {
+        int[] sortedNonNegative = CountingSort.betterCountingSort(nonNegative);
+        assertIsSorted(sortedNonNegative);
+    }
 
-	@Test
-	public void testBetterWithSomeNegative() {
-		int[] sortedSomeNegative = CountingSort.betterCountingSort(someNegative);
-		assertIsSorted(sortedSomeNegative);
-	}
+    @Test
+    public void testBetterWithSomeNegative() {
+        int[] sortedSomeNegative = CountingSort.betterCountingSort(someNegative);
+        assertIsSorted(sortedSomeNegative);
+    }
 
 
     public static void main(String[] args) {
         jh61b.junit.TestRunner.runTests("all", CountingSortTester.class);
-    }	
+    }
 } 
